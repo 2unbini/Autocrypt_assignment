0e9809456e09ac0a9295f4e19757d770270bdc7b
Eunbin Kwon <kwoneunbini@gmail.com>
2023-01-17 21:26:37 +0900

    Initial Project Commit

---
+++ Autocrypt_assignment/Autocrypt_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autocrypt_assignment
+//
+//  Created by 권은빈 on 2023/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
